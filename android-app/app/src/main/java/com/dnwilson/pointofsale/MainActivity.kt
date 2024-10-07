ec8b6ffbecb72dc37250f527f7bd56d0b4112253
Dane Wilson <dnkw@hotmail.com>
2024-10-07 12:30:11 -0400

    Add Android App
    Signed-off-by: Dane Wilson <dnkw@hotmail.com>

---
+++ android-app/app/src/main/java/com/dnwilson/pointofsale/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.dnwilson.pointofsale
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.dnwilson.pointofsale.ui.theme.PointOfSaleTheme
+import dev.hotwire.navigation.activities.HotwireActivity
+import dev.hotwire.navigation.navigator.NavigatorConfiguration
+
+class MainActivity : HotwireActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun navigatorConfigurations() = listOf(
+        NavigatorConfiguration(
+            name = "main",
+            startLocation = "https://dev-tunnel.fiyahsoft.com",
+            navigatorHostId = R.id.main_nav_host
+        )
+    )
+}
